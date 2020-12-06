bdcec9faf21b2f53038c39590cee2d8f7c020705
Meher <meherchand.63@gmail.com>
2020-12-06 11:33:49 +0530

    removed errors

--- src/test/java/mmt/Tests/NewTestClass.java
+++ src/test/java/mmt/Tests/NewTestClass.java
@@ -3,8 +3,9 @@ package mmt.Tests;
 public class NewTestClass {
 
 	public static void main(String[] args) {
-		{System.out.print("hello",updated in git);
+		{System.out.print("hello,updated in git");
 
 	}
 
+}
 }
